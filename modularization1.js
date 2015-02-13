898a89dc72ddccab86a97b8462509e54471132eb
Paul Infield-Harm <paul@infield-harm.org>
2015-02-13 14:10:11 -0500

    Compute shifts

--- modularization1.js
+++ modularization1.js
@@ -12,7 +12,7 @@ var Input = (function (Core) {
 
 
         var pos = 0;
-        var endOfWord = '†';
+        var endOfWord = '\t';
 
 
         function isWordCharacter(c) {
@@ -55,14 +55,79 @@ var Input = (function (Core) {
     return Input;
 })(Core);
 
+var CircularShift = (function (Core){
+
+    var CircularShift = {};
+
+    Core.shifts = [];
+
+    function nextLineEnd(lineIndex, lineStart) {
+        var nextLineStart = Core.lineStarts[lineIndex+1];
+
+        if (nextLineStart) {
+            if (nextLineStart[1] == 0) {
+                return [nextLineStart[0] - 1, 3];
+            } else {
+                return [nextLineStart[0], nextLineStart[1] - 1]
+            }
+        } else {
+            return [Core.inputData.length - 1, Core.inputData[Core.inputData.length - 1].length - 1]
+        }
+
+    }
+
+    function isBefore(pos, lineEnd) {
+        if (pos[0] < lineEnd[0]) return true;
+        if (pos[0] == lineEnd[0] && pos[1] <= lineEnd[1]) return true;
+        return false;
+    }
+
+    CircularShift.prepareShifts = function() {
+
+        Core.shifts = [];
+
+        for (var lineIndex = 0; lineIndex < Core.lineStarts.length; lineIndex++) {
+            var lineStart = Core.lineStarts[lineIndex];
+            var lineEnd = nextLineEnd(lineIndex, lineStart);
+
+            console.log("Start: " + lineStart);
+            console.log("End: " + lineEnd);
+
+            var pos = [lineStart[0], lineStart[1]];
+            var s = "";
+            var nextCharIsStart = true;
+            while (isBefore(pos, lineEnd)) {
+                var c = Core.inputData[pos[0]][pos[1]];
+
+
+                if (c == '\t') {
+                    nextCharIsStart = true;
+                } else {
+                    if (nextCharIsStart) {
+                        Core.shifts.push([[pos[0], pos[1]], lineIndex]);
+                    }
+                    nextCharIsStart = false;
+                }
+                pos[1] += 1;
+                if (pos[1] == 4) {
+                    pos[0] += 1;
+                    pos[1] = 0;
+                }
+            }
+        }
+    };
+
+    return CircularShift;
+})(Core);
+
 var MasterControl = (function (Input, Core) {
 
     var MasterControl = {};
 
     MasterControl.run = function () {
         Input.readLines();
-        console.log(Core.inputData);
-        console.log(Core.lineStarts);
+        CircularShift.prepareShifts();
+        console.log(Core.shifts);
     };
 
     return MasterControl;
